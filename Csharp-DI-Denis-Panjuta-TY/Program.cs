ab975ad7ca97a80f4fd2b788a248e7dd39a3e5e7
Israel Palma <isra.palma@gmail.com>
2024-09-09 13:26:50 -0600

    This is the basic way to create a class with dependencies, where the class is responsible of instantiating its own dependencies, here it's demonstrated by how the Builder class declares and instantiates it's own hammer and saw objects

--- Csharp-DI-Denis-Panjuta-TY/Program.cs
+++ Csharp-DI-Denis-Panjuta-TY/Program.cs
@@ -1,10 +1,50 @@
 ﻿namespace Csharp_DI_Denis_Panjuta_TY
 {
+
+    public class Hammer
+    {
+        public void Use() 
+        {
+            Console.WriteLine("Hammering Nails!");
+        }
+    }
+
+    public class Saw
+    {
+        public void Use()
+        {
+            Console.WriteLine("Sawing Wood!");
+        }
+    }
+
+    public class Builder
+    {
+        private Hammer _hammer;
+        private Saw _saw;
+
+        public Builder()
+        {
+            _hammer = new Hammer();
+            _saw = new Saw();
+        }
+
+        public void BuildHouse()
+        {
+            //Builder depends on the hammer and the saw
+            // builder is responsible for creating it's tools, namely
+            //  its hammer and its saw
+            _saw.Use();
+            _hammer.Use();
+            Console.WriteLine("House Built!");   
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Builder builder = new Builder();
+            builder.BuildHouse();
         }
     }
 }
