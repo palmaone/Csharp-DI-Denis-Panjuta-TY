388d12b2b751cd33e167a1588165c7dbef461f5c
Israel Palma <isra.palma@gmail.com>
2024-09-09 13:39:08 -0600

    We changed our code so that the creation or instantiation of the dependencies needed by the Builder are no longer it's responsibility, but are now the responsibility of the client program

--- Csharp-DI-Denis-Panjuta-TY/Program.cs
+++ Csharp-DI-Denis-Panjuta-TY/Program.cs
@@ -1,50 +1,54 @@
-﻿namespace Csharp_DI_Denis_Panjuta_TY
+﻿namespace Csharp_DI_Denis_Panjuta_TY;
+
+
+public class Hammer
 {
-
-    public class Hammer
+    public void Use() 
     {
-        public void Use() 
-        {
-            Console.WriteLine("Hammering Nails!");
-        }
-    }
-
-    public class Saw
-    {
-        public void Use()
-        {
-            Console.WriteLine("Sawing Wood!");
-        }
-    }
-
-    public class Builder
-    {
-        private Hammer _hammer;
-        private Saw _saw;
-
-        public Builder()
-        {
-            _hammer = new Hammer();
-            _saw = new Saw();
-        }
-
-        public void BuildHouse()
-        {
-            //Builder depends on the hammer and the saw
-            // builder is responsible for creating it's tools, namely
-            //  its hammer and its saw
-            _saw.Use();
-            _hammer.Use();
-            Console.WriteLine("House Built!");   
-        }
-    }
-
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Builder builder = new Builder();
-            builder.BuildHouse();
-        }
+        Console.WriteLine("Hammering Nails!");
     }
 }
+
+public class Saw
+{
+    public void Use()
+    {
+        Console.WriteLine("Sawing Wood!");
+    }
+}
+
+public class Builder
+{
+    private Hammer _hammer;
+    private Saw _saw;
+
+    public Builder(Hammer hammer, Saw saw)
+    {
+        _hammer = hammer;
+        _saw = saw;
+    }
+
+    public void BuildHouse()
+    {
+        //Builder depends on the hammer and the saw
+        // the client program or process is responsible for passing in
+        // the required dependencies trhoug the constructor
+        _saw.Use();
+        _hammer.Use();
+        Console.WriteLine("House Built!");   
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // the user program or process is responsible for passing in
+        // the required dependencies trhoug the constructor
+        Hammer hammer = new Hammer();
+        Saw saw = new Saw();    
+        Builder builder = new Builder(hammer, saw);
+        builder.BuildHouse();
+    }
+}
+
